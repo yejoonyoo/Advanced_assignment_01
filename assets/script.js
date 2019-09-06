1095b7c1b4907aaeb1c36a3a4285ed45c628011d
yejoonyoo <50791650+yejoonyoo@users.noreply.github.com>
2019-09-06 14:00:39 -0400

    12

---
+++ assets/script.js
@@ -0,0 +1,131 @@
+console.log("test")
+// datatypes
+// 'string'
+// number:-21
+// boolean: true false
+// array: 
+// [
+
+// 	'string', 
+// 	'another string', 
+// 	22, 
+// 	true
+// 	[
+// 		''nested',
+// 		'another',
+// 		'array'
+// 	],
+// 	false
+
+// ]
+
+// object
+// {
+// 	'key': 'value',
+// 	'anotherkey': false,
+// 	'key3' : [1, 2, 3],
+// 	'key4' : {
+// 		'key' : 'value',
+// 		'object' : {
+// 			'title' : 'another value'
+// 		}
+// 	}
+// }
+
+// object
+// [
+// 	{
+// 		'className' : 'Web Advanced',
+// 		'school': "Parsons"
+// 	},
+// 	{
+// 		'className' : 'Core Interaction',
+// 		'school': "Parsons"
+// 	}
+// ]
+
+// [1, 2, 3, 4].length
+
+// [1, 2, 3, 4] [0]
+
+
+// var myVariable = 'this is a string'
+
+// var minute = 17;
+
+// var className = 'Web Advanced';
+
+
+// var myArray =[];
+
+// myArray = [2, 3, 4];
+
+
+// var myNumber = 2;
+
+// myNumber = 3;
+
+// var myOtherNumber = 5;
+
+// var addition = (myOtherNumber + 4) / myNumber 
+// var order = myOtherNumber + 4 / myNumber 
+
+
+var myFirstValue = 5;
+var mySecondValue = 4;
+var myThirdValue = 'sample string'
+
+// 5 > 3 //true
+// 5 > 6 //false
+// 5 > 5 //false
+// 5 >= 5 //true
+// 5 <= 5 //true
+// 5 == 5 //true
+// 5 == 4 //false
+// 5 === 5 //true 
+// 5 == "5"//true
+// 5 === "5"//true 
+// 5 != 5 //false
+// 5 != 4 //true
+// 5 !== 5//false
+// 5 !== "5"//true 
+
+// if ( myFirstValue === mySecondValue) {
+
+// 	//do something
+// 	console.log('hello from if!')
+// }else{
+// 	console.log('hello from elsewhere!')
+// }
+
+
+// if ( myFirstValue === mySecondValue) {
+// 	console.log('hello from if!')
+// }
+
+// else if ( myFirstValue === 3) {
+// 	console.log("hello from if's elsewhere!")
+// }
+
+// else {
+// 	console.log('hello from elsewhere!')
+// }
+
+// if ( myFirstValue === mySecondValue && myThirdValue === 'sample string') {
+// 	console.log('hello from if!')
+// }
+
+if ( myFirstValue === mySecondValue || myThirdValue === 'sample string') {
+	console.log('hello from if!')
+}
+
+var modulus = 8 % 3; 
+
+console.log("this is the modulus:");
+
+var iteration = 0;
+console.log("first iteration 😀");
+
+iteration = iteration + 1;
+
+console.log("second iteration")
